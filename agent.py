4279adc4f3e7d8d22a3bd8e4e9e01bc9c547b41b
Yifan Xie <1029464886@qq.com>
2025-04-10 10:29:03 +0800

    规范输入输出

--- agent.py
+++ agent.py
@@ -6,6 +6,7 @@ import json
 import pandas as pd
 import time
 from io import BytesIO
+import agent_network.graph.context as ctx
 import re
 
 class excel_generator_agent(BaseAgent):
@@ -21,26 +22,32 @@ class excel_generator_agent(BaseAgent):
                 "result": "[Error] Missing required arguments: task, data."
             }
 
-        try:
-            prompt = f"""
-            你是专业的文本数据结构化处理工程师，具备以下核心能力：
-            1、精准分析理解用户所给出的文本数据。
-            2、总结相同类型数据的关键词，提取文本中的结构化数据。
-            3、 结果交付：以Json格式返回用户所关心的内容，数据结构如下
-            {{
-                "structure_data": [
-                {{"关键词1": "值1", "关键词2": "值2", "关键词3": "值3"}},
-                {{"关键词1": "值4", "关键词2": "值5", "关键词3": "值6"}},
-                {{"关键词1": "值7", "关键词2": "值8", "关键词3": "值9"}}
-                ]
-            }}
-            
-            现在用户的需求是：{task}
-            用户给出的文本数据是：
-            {data}
-            请根据上述用户需求和文本数据，完成指定的任务。只返回任务的结果，不包含解释或其他内容。
-            """
+        prompt = f"""
+        你是专业的文本数据结构化处理工程师，具备以下核心能力：
+        1、精准分析理解任务需求 和 用户所输入的文本信息。
+        2、总结相同类型数据的关键词，提取文本中的结构化数据。
+        3、结果交付：以Json格式返回用户所关心的内容，数据结构如下
+        {{
+            "structure_data": [
+            {{"关键词1": "值1", "关键词2": "值2", "关键词3": "值3"}},
+            {{"关键词1": "值4", "关键词2": "值5", "关键词3": "值6"}},
+            {{"关键词1": "值7", "关键词2": "值8", "关键词3": "值9"}}
+            ]
+        }}
+        
+        现在的任务需求是：{task}
+        用户所输入的文本信息是：{data}
+        请根据上述用户需求和文本数据，完成指定的任务。只返回任务的结果，不包含解释或其他内容。
+        """
 
+        if len(data) < 100:
+            self.log("assistant", "data内容较少")
+            return {
+                "flag": "False",
+                "message": "data内容较少"
+            }
+
+        try:
             self.add_message("user", prompt, messages)
             response = self.chat_llm(messages,
                                     api_key="sk-cf690968fd414e058f7cb0d2d3273c22",
@@ -56,14 +63,21 @@ class excel_generator_agent(BaseAgent):
             print(response_data)
             self.log("assistant", response_data)
             response_data = self._clean_response(response_data)
-            
+
             data_list = json.loads(response_data)
             df = pd.DataFrame(data_list.get("structure_data"))
             excel_buffer = BytesIO()
             with pd.ExcelWriter(excel_buffer, engine='openpyxl') as writer:
                 df.to_excel(writer, index=False, sheet_name="Sheet1")
             excel_binary_data = excel_buffer.getvalue()
+        except Exception as e:
+            self.log("assistant", f"生成Excel失败 {str(e)}")
+            return {
+                "flag": "False",
+                "message": "生成Excel失败"
+            }
 
+        try:
             file_name = time.strftime("%Y%m%d-%H%M%S") + "_ExcelGen.xlsx"
             oss.bucket.put_object(file_name, excel_binary_data)
             url = oss.bucket.sign_url('GET', file_name, 604800, slash_safe=True)
@@ -72,17 +86,17 @@ class excel_generator_agent(BaseAgent):
             self.log("assistant", url)
             # 返回
             result = {
+                "flag": "True",
+                "message": "成功生成Excel文件",
                 "result": url
             }
-
             return result
-        
         except Exception as e:
-            self.log("assistant", f"[Error] {e}")
+            self.log("assistant", f"上传OSS失败 {str(e)}")
             return {
-                "result": "[Error] " + str(e)
+                "flag": "False",
+                "message": "上传OSS失败"
             }
-
 
     def _clean_response(self, response):
         code_pattern = r'```(?:json|xml)?\s*([\s\S]*?)\s*```'
